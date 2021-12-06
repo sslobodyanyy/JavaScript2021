51c729ddc5095a0316b928a8e26b5734a05a6649
sslobodyanyy <83408973+sslobodyanyy@users.noreply.github.com>
2021-12-06 11:47:09 +0200

    Update classwork.js

--- lesson6/classwork.js
+++ lesson6/classwork.js
@@ -1,81 +1,85 @@
+// - Дано список імен.
+// let n1 = 'Harry..Potter';
+// let n2 = 'Ron---Whisley';
+// let n3 = 'Hermione__Granger';
 
-    // - Дано список імен.
-    // let n1 = 'Harry..Potter';
-    // let n2 = 'Ron---Whisley';
-    // let n3 = 'Hermione__Granger';
+// Написати функцію, яка приймає будь яке не валідне імя, та нормалізує його в наступнйи вигляд
+// let n1 = 'Harry Potter'
+// let n2 = 'Ron Whisley'
+// let n3 = 'Hermione Granger'
 
-    // Написати функцію, яка приймає будь яке не валідне імя, та нормалізує його в наступнйи вигляд
-    // let n1 = 'Harry Potter'
-    // let n2 = 'Ron Whisley'
-    // let n3 = 'Hermione Granger'
-    //
-    // let name = (string, symbol) => {
-    //     let newArr = [];
-    //     if (string.includes(symbol)) {
-    //         let a = string.split(symbol)
-    //         a.forEach((item) => {
-    //             if (item) newArr.push(item);
-    //         })
-    //         console.log(newArr.join(' '))
-    //     }
-    // }
-    // name(n3, '_');
+// let name = (string, symbol) => {
+//     let newArray = [];
+//     if (string.includes(symbol)) {
+//         let a = string.split(symbol)
+//         a.forEach((item) => {
+//             if (item) newArray.push(item);
+//         })
+//         console.log(newArray.join(' '))
+//     }
+// }
+// name(n1, '.');
+// name(n2, '-');
+// name(n3, '_')
 
 
     //  - створити функцію, яка генерує масив рандомних числових цілих значень в діапазоні від 0 до 100.
 
-    // let random = (length, num) => {
-    //     let emptyArray = [];
+    // let randomNumbers = (length, num) => {
+    //     let array = [];
     //     for (let i = 0; i < length; i++) {
-    //         emptyArray.push(Math.floor(Math.random() * num));
+    //         array.push(Math.floor(Math.random() * num));
     //     }
-    //     return emptyArray;
+    //     return array;
     // };
-    // let result = random(10, 100);
+    // let result = randomNumbers(50, 100);
     // console.log(result);
 
     // - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. Відсортувати його за допомоги sort
 
-    // // 1 варіант використовуючи попередню функцію
-    // result.sort(( a, b ) =>  a - b);
-    // console.log(result)
-    // result.sort(( a, b ) =>  b - a);
-    // console.log(result)
-
-    // // 2 варіант з новою функцією
-    //
-    // let sortRandom = (length, num) => {
+    // let sortOne = (length, num) => {
     //     let emptyArray = [];
     //     for (let i = 1; i < length; i++) {
     //         emptyArray.push(Math.floor(Math.random() * num));
     //     }
     //     return emptyArray.sort((a, b) => a - b);
+    //
     // };
-    // console.log(sortRandom(10, 100));
+    // console.log(sortOne(5, 200));
+    //
+    // let sortTwo = (length, num) => {
+    //     let emptyArray = [];
+    //     for (let i = 1; i < length; i++) {
+    //         emptyArray.push(Math.floor(Math.random() * num));
+    //     }
+    //     return emptyArray.sort((a, b) => b - a);
+    //
+    // };
+    // console.log(sortTwo(5, 200));
 
     // - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень.
     // відфільтрувати  його за допомоги filter, залишивши тільки парні числа
 
-    // let funcFilter = (length, num) => {
-    //     let emptyArray = [];
+    // let randomFilter = (length, num) => {
+    //     let array = [];
     //     for (let i = 0; i < length; i++) {
-    //         emptyArray.push(Math.floor(Math.random() * num));
+    //         array.push(Math.floor(Math.random() * num));
     //     }
-    //     return emptyArray.filter(value => value % 2 === 0);
+    //     return array.filter(value => value % 2 === 0);
     // };
-    // console.log(funcFilter(10, 100));
+    // console.log(randomFilter(5, 60));
 
     // - створити масив рандомних цілих числових значень (або згенерувати, за допомоги попередньої функції) .
     // за допомоги map та колбеку перетворити всі об'єкти в масиві на стрінгові.
 
-    // let arrayRandom = (length, num) => {
-    //     let emptyArray = [];
+    // let arrayRandomNum = (length, num) => {
+    //     let array = [];
     //     for (let i = 0; i < length; i++) {
-    //         emptyArray.push(Math.floor(Math.random() * num));
+    //         array.push(Math.floor(Math.random() * num));
     //     }
-    //     return emptyArray.map(value => value.toString());
+    //     return array.map(value => value.toString());
     // };
-    // console.log(arrayRandom(10, 100));
+    // console.log(arrayRandomNum(50, 300));
 
     // - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого,
     // або навпаки в залежності від значення аргументу direction.
@@ -87,9 +91,9 @@
     //     if (direction === 'asc') return arr.sort((a, b) => a - b);
     //     if (direction === 'desc') return arr.sort((a, b) => b - a);
     // };
-    // console.log(sortNums([1, 56, 9, -20, 2345, 34, 98, 5], 'desc') );
+    // console.log(sortNums([11, 34, 4, 55, 1000, 14, -40, 507], 'desc') );
 
-    // - є масив
+    // // - є масив
     // let coursesAndDurationArray = [
     //     {title: 'JavaScript Complex', monthDuration: 5},
     //     {title: 'Java Complex', monthDuration: 6},
@@ -98,31 +102,31 @@
     //     {title: 'FullStack', monthDuration: 7},
     //     {title: 'Frontend', monthDuration: 4}
     // ];
-
-    // -- відсортувати його за спаданням за monthDuration
-
-    // let newSort = coursesAndDurationArray.sort((a, b) => b.monthDuration - a.monthDuration);
-    // console.log(newSort);
-
-
-    // -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
-
-    // let newFilter = coursesAndDurationArray.filter((value) => {
+    //
+    // // -- відсортувати його за спаданням за monthDuration
+    //
+    // let Sort = coursesAndDurationArray.sort((a, b) => b.monthDuration - a.monthDuration);
+    // console.log(Sort);
+    //
+    //
+    // // -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+    //
+    // let Filter = coursesAndDurationArray.filter((value) => {
     //     return value.monthDuration > 5;
     // });
-    // console.log(newFilter)
+    // console.log(Filter)
 
     // - Напишите функцию cutString(str, n), которая делит строку на подстроки, состоящие из n символов.
     // document.writeln(cutString('наслаждение',3)) // [нас,лаж,ден,ие]
 
     // let cutString = (str, n) => {
-    //     const res = [];
+    //     let array = [];
     //     while (str.length) {
-    //         res.push(str.substr(0, n))
+    //         array.push(str.substr(0, n))
     //         str = str.slice(n)
     //     }
-    //     console.log(res)
-    //     return res
+    //     console.log(array)
+    //     return array
     // };
-    // document.writeln(cutString('Армагедон', 3))
+    // document.writeln(cutString('наслаждение', 3))
 
