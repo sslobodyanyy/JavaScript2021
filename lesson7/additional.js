bc59e4c44a7d711938f522962649775969739e1e
sslobodyanyy <83408973+sslobodyanyy@users.noreply.github.com>
2021-12-15 11:13:27 +0200

    Update additional.js

--- lesson7/additional.js
+++ lesson7/additional.js
@@ -28,158 +28,51 @@
 
 
 
-const Contact = function(id, name, username, email, address, phone, website, company) {
-    return {
-        id,
-        name,
-        username,
-        email,
-        address,
-        phone,
-        website,
-        company
+const contact = function(id, name, username, email, street, suite, city, zipcode, lat, lng, phone, website, companyName, catchPhrase, bs) {
+  return {
+    id,
+    name,
+    username,
+    email,
+    address: {
+      street,
+      suite,
+      city,
+      zipcode,
+      geo: {
+        lat,
+        lng
+      }
+    },
+    phone,
+    website,
+    company: {
+      name: companyName,
+      catchPhrase,
+      bs
     }
+  }
 }
 
-const Address = function(street, suite, city, zipcode, lat, lng) {
-    return {
-        street,
-        suite,
-        city,
-        zipcode,
-        geo: {
-            lat,
-            lng
-        }
-    }
-}
+const c1 = contact(
+  1,
+  'Leanne Graham',
+  'Bret',
+  'Sincere@april.biz',
+  'Kulas Light',
+  'Apt. 556',
+  'Gwenborough',
+  '92998-3874',
+  '-37.3159',
+  '81.1496',
+  '1-770-736-8031 x56442',
+  'hildegard.org',
+  'Romaguera-Crona',
+  'Multi-layered client-server neural-net',
+  'harness real-time e-markets'
+);
 
-const Company = function(name, catchPhrase, bs) {
-    return {
-        name,
-        catchPhrase,
-        bs
-    }
-}
-
-const ContactBuilder = function() {
-    let id, name, username, email, address, phone, website, company;
-
-    return {
-        id: function(value) {
-            id = value;
-            return this;
-        },
-        name: function(value) {
-            name = value;
-            return this;
-        },
-        username: function(value) {
-            username = value;
-            return this;
-        },
-        email: function(value) {
-            email = value;
-            return this;
-        },
-        address: function(value) {
-            address = value;
-            return this;
-        },
-        phone: function(value) {
-            phone = value;
-            return this;
-        },
-        website: function(value) {
-            website = value;
-            return this;
-        },
-        company: function(value) {
-            company = value;
-            return this;
-        },
-        build: function() {
-            return Contact(id, name, username, email, address, phone, website, company);
-        }
-    }
-}
-
-const AddressBuilder = function() {
-    let street, suite, city, zipcode, lat, lng;
-
-    return {
-        street: function(value) {
-            street = value;
-            return this;
-        },
-        suite: function(value) {
-            suite = value;
-            return this;
-        },
-        city: function(value) {
-            city = value;
-            return this;
-        },
-        zipcode: function(value) {
-            zipcode = value;
-            return this;
-        },
-        geo: function(latValue, lngValue) {
-            lat = latValue;
-            lng = lngValue;
-            return this;
-        },
-        build: function() {
-            return Address(street, suite, city, zipcode, lat, lng);
-        }
-    }
-}
-
-const CompanyBuilder = function() {
-    let name, catchPhrase, bs;
-
-    return {
-        name: function(value) {
-            name = value;
-            return this;
-        },
-        catchPhrase: function(value) {
-            catchPhrase = value;
-            return this;
-        },
-        bs: function(value) {
-            bs = value;
-            return this;
-        },
-        build: function() {
-            return Company(name, catchPhrase, bs);
-        }
-    }
-}
-
-const c = ContactBuilder()
-    .id(1)
-    .name('Leanne Graham')
-    .username('Bret')
-    .email('Sincere@april.biz')
-    .address(AddressBuilder()
-        .street('Kulas Light')
-        .suite('Apt. 556')
-        .city('Gwenborough')
-        .zipcode('92998-3874')
-        .geo('-37.3159', '81.1496')
-        .build())
-    .phone('1-770-736-8031 x56442')
-    .website('hildegard.org')
-    .company(CompanyBuilder()
-        .name('Romaguera-Crona')
-        .catchPhrase('Multi-layered client-server neural-net')
-        .bs('harness real-time e-markets')
-        .build())
-    .build();
-
-console.log(c);
-
+console.log(c1);
 // =========================================================
 // -  Створити функцію конструктор / клас  який описує об'єкт тегу
 // Поля :
@@ -214,125 +107,73 @@ console.log(c);
 // }
 
 
-const Tag = function(titleOfTag, action, attrs) {
-    return {
-        titleOfTag,
-        action,
-        attrs
-    }
+const tag = (titleOfTag, action, ...attrs) => {
+  return {
+    titleOfTag,
+    action,
+    attrs
+  }
 }
 
-const Attribute = function(titleOfAttr, actionOfAttr) {
-    return {
-        titleOfAttr,
-        actionOfAttr
-    }
+const attr = (titleOfAttr, actionOfAttr) => {
+  return {
+    titleOfAttr,
+    actionOfAttr
+  }
 }
 
-const TagBuilder = function () {
-    let titleOfTag, action, attrs;
+const t1 = tag(
+  'a',
+  'Тег <a> является одним из важных элементов HTML и предназначен для создания ссылок. В зависимости от присутствия атрибутов name или href тег <a> устанавливает ссылку или якорь. Якорем называется закладка внутри страницы, которую можно указать в качестве цели ссылки. При использовании ссылки, которая указывает на якорь, происходит переход к закладке внутри веб-страницы.',
+  attr('accesskey', 'Активация ссылки с помощью комбинации клавиш.'),
+  attr('coords', 'Устанавливает координаты активной области.')
+);
 
-    return {
-        titleOfTag: function(value) {
-            titleOfTag = value;
-            return this;
-        },
-        action: function(value) {
-            action = value;
-            return this;
-        },
-        attrs: function(values) {
-            attrs = values;
-            return this;
-        },
-        build: function() {
-            return Tag(titleOfTag, action, attrs);
-        }
-    }
-}
+console.log(t1);
+const t2 = div(
+  'div',
+  'Элемент <div> является блочным элементом и предназначен для выделения фрагмента документа с целью изменения вида содержимого. Как правило, вид блока управляется с помощью стилей. Чтобы не описывать каждый раз стиль внутри тега, можно выделить стиль во внешнюю таблицу стилей, а для тега добавить атрибут class или id с именем селектора.'  attr('accesskey', 'Активация ссылки с помощью комбинации клавиш.'),
+  attr('align', 'Задает выравнивание содержимого тега <div>.')
+);
+console.log(t2);
+const t3 = h1(
+  'h1',
+  'тег <h1> представляет собой наиболее важный заголовок первого уровня'  attr('accesskey', 'Активация ссылки с помощью комбинации клавиш.'),
+   attr('acceskey', 'Позволяет получить доступ к элементу с помощью заданного сочетания клавиш.')
+);
+console.log(t3);
 
-const AttributesBuilder = function() {
-    let array = [];
+const t4 = span(
+  'span',
+  'Тег <span> предназначен для определения строчных элементов документа.',
+   attr('contextmenu', 'Устанавливает контекстное меню для элемента.')
+);
+console.log(t4);
 
-    return {
-        add: function(titleOfAttr, actionOfAttr) {
-            array.push({titleOfAttr, actionOfAttr});
-            return this;
-        },
-        build: function() {
-            return array;
-        }
-    }
-}
+const t5 = input(
+  'input',
+  'Тег <input> предназначен для определения строчных элементов документа.',
+   attr('lang', 'Браузер использует значение параметра для правильного отображения некоторых национальных символов.')
+);
+console.log(t5);
 
-console.log(TagBuilder()
-    .titleOfTag('a')
-    .action('Тег <a> является одним из важных элементов HTML и предназначен для создания ссылок. В зависимости от присутствия атрибутов name или href тег <a> устанавливает ссылку или якорь. Якорем называется закладка внутри страницы, которую можно указать в качестве цели ссылки. При использовании ссылки, которая указывает на якорь, происходит переход к закладке внутри веб-страницы.')
-    .attrs(AttributesBuilder()
-        .add('accesskey', 'Активация ссылки с помощью комбинации клавиш.')
-        .add('coords', 'Устанавливает координаты активной области.')
-        .build())
-    .build());
+const t6 = form(
+  'form',
+  'Тег <form> устанавливает форму на веб-странице. ',
+   attr('action', 'Адрес программы или документа, который обрабатывает данные формы.')
+);
+console.log(t6);
 
-console.log(TagBuilder()
-    .titleOfTag('div')
-    .action('Элемент <div> является блочным элементом и предназначен для выделения фрагмента документа с целью изменения вида содержимого. Как правило, вид блока управляется с помощью стилей. Чтобы не описывать каждый раз стиль внутри тега, можно выделить стиль во внешнюю таблицу стилей, а для тега добавить атрибут class или id с именем селектора.')
-    .attrs(AttributesBuilder()
-        .add('align', 'Задает выравнивание содержимого тега <div>.')
-        .add('title', 'Добавляет всплывающую подсказку к содержимому.')
-        .build())
-    .build());
+const t7 = option(
+  'option',
+  'Тег <option> определяет отдельные пункты списка, создаваемого с помощью контейнера <select>.',
+   attr('action', 'Адрес программы или документа, который обрабатывает данные формы.')
+);
+console.log(t7);
 
-console.log(TagBuilder()
-    .titleOfTag('h1')
-    .action('тег <h1> представляет собой наиболее важный заголовок первого уровня')
-    .attrs(AttributesBuilder()
-        .add('acceskey', 'Позволяет получить доступ к элементу с помощью заданного сочетания клавиш.')
-        .add('class', 'Определяет имя класса, которое позволяет связать тег со стилевым оформлением.')
-        .build())
-    .build());
-
-console.log(TagBuilder()
-    .titleOfTag('span')
-    .action('Тег <span> предназначен для определения строчных элементов документа.')
-    .attrs(AttributesBuilder()
-        .add('contextmenu', 'Устанавливает контекстное меню для элемента.')
-        .add('lang', 'Браузер использует значение параметра для правильного отображения некоторых национальных символов.')
-        .build())
-    .build());
-
-console.log(TagBuilder()
-    .titleOfTag('input')
-    .action('Тег <span> предназначен для определения строчных элементов документа.')
-    .attrs(AttributesBuilder()
-        .add('contextmenu', 'Устанавливает контекстное меню для элемента.')
-        .add('lang', 'Браузер использует значение параметра для правильного отображения некоторых национальных символов.')
-        .build())
-    .build());
-
-console.log(TagBuilder()
-    .titleOfTag('form')
-    .action('Тег <form> устанавливает форму на веб-странице. ')
-    .attrs(AttributesBuilder()
-        .add('action', 'Адрес программы или документа, который обрабатывает данные формы.')
-        .add('name', 'Имя формы.')
-        .build())
-    .build());
-
-console.log(TagBuilder()
-    .titleOfTag('option')
-    .action('Тег <option> определяет отдельные пункты списка, создаваемого с помощью контейнера <select>.')
-    .attrs(AttributesBuilder()
-        .add('disabled', 'Заблокировать для доступа элемент списка.')
-        .add('label', 'Указание метки пункта списка.')
-        .build())
-    .build());
-
-console.log(TagBuilder()
-    .titleOfTag('select')
-    .action('Тег <select> позволяет создать элемент интерфейса в виде раскрывающегося списка, а также список с одним или множественным выбором')
-    .attrs(AttributesBuilder()
-        .add('multiple', 'Позволяет одновременно выбирать сразу несколько элементов списка.')
-        .add('size', 'Количество отображаемых строк списка.')
-        .build())
-    .build());
+const t8 = select(
+  'select',
+  'Тег <select> позволяет создать элемент интерфейса в виде раскрывающегося списка, а также список с одним или множественным выбором',
+   attr('multiple', 'Позволяет одновременно выбирать сразу несколько элементов списка.')
+);
+console.log(t8);
