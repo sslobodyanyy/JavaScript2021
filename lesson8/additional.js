157d5e5a8b89ec4d5073006a5763c0c585fb8446
sslobodyanyy <83408973+sslobodyanyy@users.noreply.github.com>
2021-12-15 12:06:47 +0200

    Update additional.js

--- lesson8/additional.js
+++ lesson8/additional.js
@@ -1,1 +1,19 @@
+const getClassName = (elements, arr) => {
+                for (let i = 0; i < elements.length; i++) {
+                    const node = elements[i];
 
+                    if (node.classList.length > 0) {
+                            for (let n = 0; n < node.classList.length; n++) {
+                                arr.push(node.classList.item(n));
+                            }
+                        }
+
+                    if (node.hasChildNodes()) {
+                        getClassName(node, arr)
+                    }
+                }
+            }
+            
+            const allClasses = [];
+            getClassName(document.body.querySelectorAll('*'), allClasses);
+            console.log(allClasses);
